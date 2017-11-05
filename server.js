878bc7c0d2ba315710d69b32c34af84c5106ad29
Lingesh <Lingesh46@gmail.com>
2017-11-05 21:20:15 +0530

    files upadtes

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const hbs = require('hbs');
 
+const port = process.env.PORT || 5000;
 var app = express();
 
 app.set('view engine','hbs');
@@ -21,4 +22,4 @@ app.get('/',(req,res) => {
   });
 })
 
-app.listen(5000)
+app.listen(port)
